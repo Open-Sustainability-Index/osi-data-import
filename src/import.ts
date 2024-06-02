1545d8571bb97c64a70f26081154d2f3f61d97b8
Tom Söderlund <tom@tomsoderlund.com>
2024-06-02 12:44:18 +0200

    Import emissions

--- src/import.ts
+++ src/import.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { Client } from 'pg';
 import * as dotenv from 'dotenv';
-import { toSnakeCase, checkHeaders, tableHeaders } from './utils';
+import { toSnakeCase, checkHeaders, tableHeaders, TableHeader } from './utils';
 const csv = require('csv-parser'); // Use require instead of import
 
 dotenv.config();
@@ -19,63 +19,76 @@ const client = new Client({
 
 client.connect();
 
-async function deleteAllData () {
+async function deleteAllData() {
   console.log('Deleting all data...');
   await client.query('DELETE FROM target;');
   await client.query('DELETE FROM emission;');
   await client.query('DELETE FROM commitment;');
-  await client.query('DELETE FROM company;');
+  // await client.query('DELETE FROM company;');
 }
 
-async function importAll () {
+async function importAll() {
   await deleteAllData();
-  await importCsvFileToPostgres(client, 'company', tableHeaders.company, './data/companies.csv');
+  // await importCsvFileToPostgres(client, 'company', tableHeaders.company, './data/companies.csv');
+  await importCsvFileToPostgres(client, 'emission', tableHeaders.emission, './data/emissions.csv', 'company_name');
 }
 
-async function importCsvFileToPostgres (
+async function importCsvFileToPostgres(
   client: Client,
   tableName: string,
-  requiredHeaders: string[],
+  requiredHeaders: TableHeader[],
   csvFilePath = './data/companies.csv',
   previewField = 'name',
 ) {
   const results: any[] = [];
-  
+
   fs.createReadStream(csvFilePath)
     .pipe(csv())
     .on('headers', (headers: string[]) => {
       const snakeCaseHeaders = headers.map(toSnakeCase);
       const missingHeaders = checkHeaders(requiredHeaders, snakeCaseHeaders);
-  
+
       if (missingHeaders.length > 0) {
         console.error(`Missing headers: ${missingHeaders.join(', ')}`);
         process.exit(1);
       }
-  
+
       // console.log('Headers in CSV:', snakeCaseHeaders);
     })
     .on('data', (data: any) => results.push(data))
     .on('end', async () => {
       try {
+        let rowIndex = 0;
         for (const row of results) {
-          // Filter and map row data to required headers
-          const filteredRow = Object.keys(row).reduce((acc: Record<string, any>, header) => {
-            const snakeCaseHeader = toSnakeCase(header);
-            if (requiredHeaders.includes(snakeCaseHeader) && row[header] !== undefined) { // Use the original header name to access the row data
-              acc[snakeCaseHeader] = row[header];
-            }
-            return acc;
-          }, {} as Record<string, any>);
-  
-          const columns = Object.keys(filteredRow).join(', ');
-          const values = Object.values(filteredRow);
-          const valuePlaceholders = values.map((_, i) => `$${i + 1}`).join(', ');
-  
-          console.log(`Importing ${tableName}:`, filteredRow[previewField]);
-          const query = `INSERT INTO ${tableName} (${columns}) VALUES (${valuePlaceholders})`;  
-          await client.query(query, values);
+          rowIndex++;
+          try {
+            // Filter and map row data to required headers
+            const filteredRow = Object.keys(row).reduce((acc: Record<string, any>, header) => {
+              const snakeCaseHeader = toSnakeCase(header);
+              if (requiredHeaders.map(header => header.name).includes(snakeCaseHeader) && row[header] !== undefined) { // Use the original header name to access the row data
+                const requiredHeader = requiredHeaders.find(header => header.name === snakeCaseHeader);
+                const value = row[header] === ''
+                  ? null
+                  : requiredHeader ?.type === 'number'
+                    ? parseFloat(row[header])
+                    : row[header];
+                acc[snakeCaseHeader] = value;
+              }
+              return acc;
+            }, {} as Record<string, any>);
+
+            const columns = Object.keys(filteredRow).join(', ');
+            const values = Object.values(filteredRow);
+            const valuePlaceholders = values.map((_, i) => `$${i + 1}`).join(', ');
+
+            const query = `INSERT INTO ${tableName} (${columns}) VALUES (${valuePlaceholders})`;
+            await client.query(query, values);
+            console.log(`Imported ${tableName}:`, filteredRow[previewField]);
+          } catch (error: any) {
+            console.warn('Error inserting row:', rowIndex, error?.message);
+          }
         }
-  
+
         console.log('Data imported successfully.');
       } catch (error) {
         console.error('Error inserting data:', error);
--- src/utils.ts
+++ src/utils.ts
@@ -5,25 +5,82 @@ export function toSnakeCase(str: string): string {
   return _.snakeCase(str);
 }
 
+export interface TableHeader {
+  name: string;
+  type?: 'string' | 'number' | 'date' | 'boolean';
+}
+
 // Required headers checklist
-export const tableHeaders = {
+export const tableHeaders: Record<string, TableHeader[]> = {
   company: [
-    'name',
-    'industry',
-    // 'isic',
-    'lei',
-    'company_url',
-    'source_reports_page',
-    'hq_country',
-    // 'sbt_status',
-    // 'sbt_near_term_year',
-    // 'sbt_near_term_target',
-    // 'net_zero_year',
+    { name: 'name' },
+    { name: 'industry' },
+    // { name: 'isic' },
+    { name: 'lei' },
+    { name: 'company_url' },
+    { name: 'source_reports_page' },
+    { name: 'hq_country' },
+    // { name: 'sbt_status' },
+    // { name: 'sbt_near_term_year' },
+    // { name: 'sbt_near_term_target' },
+    // { name: 'net_zero_year' },
+  ],
+  emission: [
+    { name: 'company_name' },
+    { name: 'year' },
+    { name: 'fiscal_year' },
+    { name: 'industry' },
+    { name: 'isic_rev_4' },
+    { name: 'hq_country_move' },
+    { name: 'scope_1', type: 'number' },
+    { name: 'scope_2_market_based', type: 'number' },
+    { name: 'scope_2_location_based', type: 'number' },
+    { name: 'scope_2_unknown', type: 'number' },
+    { name: 'total_scope_3', type: 'number' },
+    { name: 'total_emission_market_based' },
+    { name: 'total_emission_location_based' },
+    { name: 'total_reported_emission_scope_1_2', type: 'number' },
+    { name: 'total_reported_emission_scope_1_2_3', type: 'number' },
+    { name: 'cat_1', type: 'number' },
+    { name: 'cat_2', type: 'number' },
+    { name: 'cat_3', type: 'number' },
+    { name: 'cat_4', type: 'number' },
+    { name: 'cat_5', type: 'number' },
+    { name: 'cat_6', type: 'number' },
+    { name: 'cat_7', type: 'number' },
+    { name: 'cat_8', type: 'number' },
+    { name: 'cat_9', type: 'number' },
+    { name: 'cat_10', type: 'number' },
+    { name: 'cat_11', type: 'number' },
+    { name: 'cat_12', type: 'number' },
+    { name: 'cat_13', type: 'number' },
+    { name: 'cat_14', type: 'number' },
+    { name: 'cat_15', type: 'number' },
+    { name: 'all_cats', type: 'number' },
+    { name: 'upstream_scope_3', type: 'number' },
+    { name: 'share_upstream_of_scope_3', type: 'number' },
+    { name: 'scope_1_share_of_total_upstream_emissions', type: 'number' },
+    { name: 'total_upstream_emissions', type: 'number' },
+    { name: 'revenue', type: 'number' },
+    { name: 'currency' },
+    { name: 'revenue_million', type: 'number' },
+    { name: 'cradle_to_gate', type: 'number' },
+    { name: 'ghg_standard' },
+    { name: 'emission_intensity' },
+    { name: 'emission_page' },
+    { name: 'page_revenue' },
+    { name: 'publication_date' },
+    { name: 'source_emisions_page_move' },
+    { name: 'source_emission_link' },
+    { name: 'source_emission_report' },
+    { name: 'source_revenue' },
+    { name: 'source_revenue_link' },
+    { name: 'status' },
   ]
 };
 
 // Check for missing headers
-export function checkHeaders(requiredHeaders: string[], headers: string[]): string[] {
-  const missingHeaders = requiredHeaders.filter(header => !headers.includes(header));
-  return missingHeaders;
+export function checkHeaders(requiredHeaders: TableHeader[], headers: string[]): string[] {
+  const missingHeaders = requiredHeaders.filter(header => !headers.includes(header.name));
+  return missingHeaders.map(header => header.name);
 }
